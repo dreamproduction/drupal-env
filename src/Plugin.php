b9f0531c8982d9c9ffed7a923ef9a03570b319fd
claudiuc <claudiu.chirilov@dreamproduction.ro>
2019-06-11 16:48:34 +0300

    Initial project commit.

---
+++ src/Plugin.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace DreamProduction\Composer;
+
+use Composer\Composer;
+use Composer\Util\ProcessExecutor;
+use Composer\Plugin\PluginInterface;
+use Composer\IO\IOInterface;
+
+
+class Plugin implements PluginInterface {  
+  /**
+   * @var Composer $composer
+   */
+  protected $composer;
+  /**
+   * @var IOInterface $io
+   */
+  protected $io;
+  /**
+   * @var EventDispatcher $eventDispatcher
+   */
+  protected $eventDispatcher;
+  /**
+   * @var ProcessExecutor $executor
+   */
+  protected $executor;
+
+	public function activate(Composer $composer, IOInterface $io) {
+	    $this->composer = $composer;
+	    $this->io = $io;
+	    $this->eventDispatcher = $composer->getEventDispatcher();
+	    $this->executor = new ProcessExecutor($this->io);
+    }
+}
