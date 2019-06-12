0e93274742d1fba53608d64e7630dd2996fcf3fa
claudiuc <claudiu.chirilov@dreamproduction.ro>
2019-06-12 12:32:17 +0300

    Fix invalid code.

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # drupal-env
 Provides environment-based files mapping.
+
+# How to use
+## 1. Install the package
+`$ composer require dreamproduction/drupal-env`
+
+## 2. Add/Change composer scripts
+First we define a new entry under the **extra** key of the root **composer.json** file named **drupal-env** then add a new entry for each file that needs to be replaced. For example:
+
+**composer.json**
+```javascript
+"extra": {
+   ...
+   "drupal-env": {
+      ".htaccess": {
+         "dev": ".htaccess.dev",
+         "stage": ".htaccess.stage",
+         "master": ".htaccess.master",
+         ...
+         "<branch_name>: "<source_file>",
+       }
+    }
+    ....
+ }
+ ....
+ ```
+
+When bundling with composer-boilerplate project you have to modify the **/vendor/dreamproduction/composer-boilerplate/composer.settings.json**  file as such:
+
+**vendor/dreamproduction/composer-boilerplate/composer.settings.json**
+```javascript
+"scripts": {
+    ...
+    "post-install-cmd": [
+       "@composer run-script drupal-scaffold",
+       "@composer run-script drupal-env"
+    ],
+    ...
+    "drupal-env": [
+        "DreamProduction\\Composer\\DrupalEnv::postUpdate"
+    ],
+    ...
+```
+## 3. Create a GIT branch-based file
+You need to create a git branch-based file mapping which will replace the target file as defined in the **extra** area of your **composer.json** root file.
--- src/DrupalEnv.php
+++ src/DrupalEnv.php
@@ -39,38 +39,38 @@ class DrupalEnv implements PluginInterface {
   }
 
   /**
-   * Returns an array of event names this subscriber wants to listen to.
-   */
-  public static function getSubscribedEvents() {
-    return array(
-      PackageEvents::POST_PACKAGE_INSTALL => array('postUpdate', 20),
-      PackageEvents::POST_PACKAGE_UPDATE => array('postUpdate', 20),
-    );
-  }
-
-  /**
    * Callback function that is triggered on package install or update.
    */
-  public function postUpdate(Event $event) {
-    $branch_name = $this->executeCommand('git -C %s rev-parse --abbrev-ref HEAD', getcwd());
+  public static function postUpdate(Event $event) {
+    $composer = $event->getComposer();
+    $io = $event->getIO();
+    $executor = new ProcessExecutor($io);
+    $command = 'git -C %s rev-parse --abbrev-ref HEAD';
+    $branch_result = self::executeCommand($executor, $command, getcwd());
+    $branch_name = trim($branch_result);
     $extra = $event->getComposer()->getPackage()->getExtra();
+
     if (isset($extra['drupal-env'])) {  
       $drupal_env_extra = $extra['drupal-env'];
       foreach ($drupal_env_extra as $filename => $filename_mapping) {
-        $file_path = $this->getFilePath($event, $filename);
+        $file_path = self::getFilePath($event, $filename);
+        if (!isset($filename_mapping[$branch_name])) {
+          $io->write('<comment>DrupalEnv: no actions taken for branch '. trim($branch_name) . '</comment>');
+          continue;
+        }
         $source_filename = $filename_mapping[$branch_name];
-        $source_filepath = $this->getFilePath($event, $source_filename);
+        $source_filepath = self::getFilePath($event, $source_filename);
         if (!file_exists($file_path)) {
-          $this->io->write('<error>Skipping file ' . $file_path . '. File does not exists.</error>');
+          $io->write('<error>Skipping file ' . $file_path . '. File does not exists.</error>');
         }
         else {
           if (!file_exists($source_filepath)) {
-            $this->io->write('<error>Source file ' . $source_filename . ' does not exists. Skipping...</error>');
+            $io->write('<error>Source file ' . $source_filename . ' does not exists. Skipping...</error>');
           }
           else {
             // Replace the destinatio file with the source file.
             if (!copy($source_filepath, $file_path)) {
-              $this->io->write('<error>Cannot copy ' . $source_filename . ' to ' . $filepath . '</error>');
+              $io->write('<error>Cannot copy ' . $source_filename . ' to ' . $filepath . '</error>');
             }
           }
         }
@@ -81,8 +81,8 @@ class DrupalEnv implements PluginInterface {
   /**
    * Returns the full path to a provided filename relative to the drupal directory.
    */
-  protected function getFilePath(Event $event, $filename) {
-    $core_path = $this->getDrupalCorePath($event);
+  protected static function getFilePath(Event $event, $filename) {
+    $core_path = self::getDrupalCorePath($event);
     $filepath = getcwd() . DIRECTORY_SEPARATOR . $core_path . $filename;
     return $filepath;
   }
@@ -90,7 +90,7 @@ class DrupalEnv implements PluginInterface {
   /**
    * Returns the path to the drupal core.
    */
-  protected function getDrupalCorePath(Event $event) {
+  protected static function getDrupalCorePath(Event $event) {
     $composer = $event->getComposer();
     $repositoryManager = $composer->getRepositoryManager();
     $installationManager = $composer->getInstallationManager();
@@ -112,9 +112,12 @@ class DrupalEnv implements PluginInterface {
    * @param string $cmd
    * @return bool
    */
-  protected function executeCommand($cmd) {
+  protected static function executeCommand($executor, $cmd) {
     // Shell-escape all arguments except the command.
     $args = func_get_args();
+    // Remove first and second argument.
+    array_shift($args);
+    // Escape the rest.
     foreach ($args as $index => $arg) {
       if ($index !== 0) {
         $args[$index] = escapeshellarg($arg);
@@ -124,19 +127,7 @@ class DrupalEnv implements PluginInterface {
     // And replace the arguments.
     $command = call_user_func_array('sprintf', $args);
     $output = '';
-    if ($this->io->isVerbose()) {
-      $this->io->write('<comment>' . $command . '</comment>');
-      $io = $this->io;
-      $output = function ($type, $data) use ($io) {
-        if ($type == Process::ERR) {
-          $io->write('<error>' . $data . '</error>');
-        }
-        else {
-          $io->write('<comment>' . $data . '</comment>');
-        }
-      };
-    }
-    $this->executor->execute($command, $output);
+    $executor->execute($command, $output);
     return $output;
   }
 }
